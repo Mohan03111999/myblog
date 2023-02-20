230302353931409a3a33966620f6ff35438f44a4
Mohan03111999 <mohanrajavelu1234@gmail.com>
2023-02-20 21:39:54 +0530

    PostDTO data transfer object to hide the JPA entity to directly be modified

---
+++ src/main/java/com/example/springrest/myblog/payload/PostDTO.java
@@ -0,0 +1,11 @@
+package com.example.springrest.myblog.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+}
